dbf8afa5cc2a8dd320270b6bf8747e5900da2140
Patrick Gabbidon <PGnOG@Patricks-MacBook-Pro.local>
2014-06-07 22:43:22 -0400

    created python script and started menu

---
+++ Shortcuts.py
@@ -0,0 +1,22 @@
+# SHORTCUTS
+# By: Patrick Gabbidon & Jacob [insert last name here]
+#
+# This Python Script when executed brings up a menu of various mini tutorials for GitHub, Python..
+
+#Introduction
+print "Welcome to Shortcuts v0.0"
+print "Created June 7, 2014"
+print
+
+#Variable Declaration
+choice = ''
+
+#Main Menu
+def menu():
+	print "MENU"
+	print "Please choose one of the following mini tutorials: "
+		
+
+
+while choice != 'x':
+	menu()
